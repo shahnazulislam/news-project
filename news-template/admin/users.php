d12f0f14cf96bd5d9f85873e3077432f4d7094b4
shahnazulislam <mdshahnazulislam@gmail.com>
2022-11-27 15:50:55 +0600

    modify pagination

--- news-template/admin/users.php
+++ news-template/admin/users.php
@@ -11,7 +11,15 @@
                 <div class="col-md-12">
                 <?php
                         include "config.php";
-                        $sql = "SELECT * FROM user ORDER BY -user_id DESC";
+                        $limit = 3;
+                        if (isset($_GET['page'])) {
+                            $page = $_GET['page'];
+                        }else{
+                            $page = 1;
+                        }
+                        $offset = ($page - 1) * $limit;
+
+                        $sql = "SELECT * FROM user ORDER BY user_id DESC LIMIT $offset, $limit";
                         $result = mysqli_query($conn, $sql) or die('Query error');
 
                         if (mysqli_num_rows($result) > 0) {
@@ -52,26 +60,41 @@
                     </table>
                         <?php
                         }
+
+                        $sql1 = "SELECT * FROM user";
+                        $result1 = mysqli_query($conn, $sql1) or die('Query faild');
+
+                        if (mysqli_num_rows($result1) > 0) {
+                            $total_record = mysqli_num_rows($result1);
+                            $total_page = ceil($total_record / $limit);
+
+                            echo "<ul class='pagination admin-pagination'>";
+                                if ($page > 1) {
+                                    echo '<li><a href="users.php?page='.($page - 1).'">prev</a></li>';
+                                }
+
+                            for ($i = 1; $i <= $total_page; $i++) { 
+                                if ($i == $page) {
+                                    $active = 'active';
+                                }else{
+                                    $active = '';
+                                }
+                                echo '<li class = "'.$active.'"><a href="users.php?page='.$i.'">'.$i.'</a></li>';
+                            }
+                            if ($total_page > $page) {
+                                    echo '<li><a href="users.php?page='.($page + 1).'">next</a></li>';
+                                }
+                            echo '</ul>';
+                        }
+
                         ?>
-                    <ul class='pagination admin-pagination'>
-                        <li class="active"><a>1</a></li>
-                        <li><a>2</a></li>
-                        <li><a>3</a></li>
-                    </ul>
+                    
+                        <!-- <li class="active"><a>1</a></li>
+                        
+                        <li><a>3</a></li> -->
+                    
                 </div>
             </div>
         </div>
     </div>
-<?php include "header.php"; ?>
-
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
-    <input type="text" placeholder="hellow world">
-</body>
-</html>
+<?php include "footer.php"; ?>
